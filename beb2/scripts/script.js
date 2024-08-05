24ae138ffb7cd7341e4e0a199187bb33e99dcb21
Bruno Fernandes <fernandesbruno01384@gmail.com>
2024-08-05 19:58:16 -0300

    Updated logos and initialized header

--- beb2/scripts/script.js
+++ beb2/scripts/script.js
@@ -7,14 +7,14 @@ let logoSpan = document.querySelectorAll('.logo');
 addEventListener('DOMContentLoaded', ()=>{
     setTimeout(()=>{
 
-        /* Makes the logo appear the screen */
+        // Makes the logo appear the screen
         logoSpan.forEach((span, idx)=>{
             setTimeout(()=>{
                 span.classList.add('active');
             }, (idx + 1) * 300);
         });
 
-        /* Makes the slogan go off the screen */
+        // Makes the slogan go off the screen
         setTimeout(()=>{
             logoSpan.forEach((span, idx)=>{
                 setTimeout(()=>{
@@ -24,9 +24,91 @@ addEventListener('DOMContentLoaded', ()=>{
             })
         },1100);
 
-        /* Makes the splash screen go off the screen */
+        // Makes the splash screen go off the screen
         setTimeout(()=>{
             intro.style.top = '-100vh';
         },1900);
     });
 });
+
+    /* == == Header == == */
+
+// Show menu
+const showMenu = (toggleId, navId) =>{
+   const toggle = document.getElementById(toggleId), nav = document.getElementById(navId);
+
+   toggle.addEventListener('click', () =>{
+       // Add show_menu class to nav menu
+       nav.classList.toggle('show_menu');
+
+       // Add show-icon to show and hide menu icon
+       toggle.classList.toggle('show_icon');
+   });
+}
+
+showMenu('nav_toggle','nav_menu');
+
+// Show dropdown menu
+const dropdownItems = document.querySelectorAll('.drop_item');
+
+// 1. Select each dropdown item
+dropdownItems.forEach((item) => {
+    const dropdownButton = item.querySelector('.drop__btn');
+
+    // 2. Select each button click
+    dropdownButton.addEventListener('click', () => {
+
+        // 7. Select the current show_drop class
+        const showDropdown = document.querySelector('.show_drop');
+
+        // 5. Call the toggleItem function
+        toggleItem(item);
+
+        // 8. Remove the show_dropdown class from other items
+        if (showDropdown && showDropdown !== item) {
+            toggleItem(showDropdown);
+        }
+    });
+})
+
+// 3. Create a function to display the dropdown
+const toggleItem = (item) => {
+
+    // 3.1. Select each dropdown content
+    const dropdownContainer = item.querySelector('.drop__container');
+
+    // 6. If the same item contains the show_drop class, remove
+    if (item.classList.contains('show_drop')) {
+        dropdownContainer.removeAttribute('style');
+        item.classList.remove('show_drop');
+    } else {
+
+        // 4. Add the maximum heigh to the dropdown content and add the show_drop class
+        dropdownContainer.style.height = dropdownContainer.scrollHeight + 'px';
+        item.classList.add('show_drop');
+    }
+}
+
+// Delete dropdown styles
+
+const mediaQuery = matchMedia('(min-width: 1118px)'), dropdownContainer = document.querySelectorAll('.drop__container');
+
+// Function to remove dropdown styles in mobile mode when browser resizes
+const removeStyle = () => {
+
+    // Validate if the media query raches 1118px
+    if (mediaQuery.matches) {
+        
+        // Remove the dropdown container height style
+        dropdownContainer.forEach((e) => {
+            e.removeAttribute('style');
+        });
+
+        // Remove the show_drop class from dropdown item
+        dropdownItems.forEach((e) => {
+            e.classList.remove('show_drop');
+        });
+    }
+}
+
+addEventListener('resize', removeStyle);
