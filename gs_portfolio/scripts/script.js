ad0fb62f0e4ef17279fbc6180afd0b7bf03af477
Bruno Fernandes <fernandesbruno01384@gmail.com>
2024-10-21 13:52:51 -0300

    Complete Portfolio Section

--- gs_portfolio/scripts/script.js
+++ gs_portfolio/scripts/script.js
@@ -147,3 +147,26 @@ themeBtn.addEventListener('click', () => {
     localStorage.setItem('selected_theme', getCurrentTheme());
     localStorage.setItem('selected_icon', getCurrentIcon());
 });
+
+/* == == == == Portfolio Filter == == == == */
+$(document).ready(function() {
+	$(".filter .item").click(function() {
+		$(this).addClass("active").siblings().removeClass("active")
+
+		$(".itens").fadeOut()
+		setTimeout(function() {
+			$(".itens").fadeIn()
+		}, 0)
+
+		let value = $(this).attr("data-filter")
+
+		setTimeout(function() {
+			if (value === "todos") {
+				$(".itens .item").show("500")
+			} else {
+				$(".itens .item").not("." + value).hide("500")
+				$(".itens .item").filter("." + value).show("500")
+			}
+		}, 0)
+	})
+})
