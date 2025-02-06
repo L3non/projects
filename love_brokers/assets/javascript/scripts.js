8c7aa153adf03520e63e5cbb424cfb46deaba9fe
Bruno Fernandes <fernandesbruno01384@gmail.com>
2025-02-06 19:36:02 -0300

    Finalização da seção hero, adição do modo escuro, estilização da scrollbar e adição da animação scroll reveal

--- love_brokers/assets/javascript/scripts.js
+++ love_brokers/assets/javascript/scripts.js
@@ -30,19 +30,44 @@ addEventListener('DOMContentLoaded', ()=>{
     });
 });
 
+/* == == == Scroll reveal == == == */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: '2500',
+    delay: '2400',
+    // reset: true, // Animations repeat
+});
+
+sr.reveal(`.home_data`);
+sr.reveal(`#home_images`, {delay: 1800})
+/*
+sr.reveal(`.services_card`, {interval: 100})
+sr.reveal(`.discount_data`, {origin: 'left'})
+sr.reveal(`.discount_images`, {origin: 'right'})
+*/
+
+/* == == == Scroll up == == == */
+const scrollUp = () => {
+    const scrollUp = document.querySelector('#scroll_up');
+    // When the scroll is higher than 350 viewport height, add the show_scroll class to the a tag with the scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add('show_scroll') : scrollUp.classList.remove('show_scroll');
+}
+addEventListener('scroll', scrollUp);
+
 /* == == == Header == == == */
 const navMenu = document.getElementById('nav_menu'),
     navToggle = document.getElementById('nav_toggle'),
-    navClose = document.getElementById('nav_close')
+    navClose = document.getElementById('nav_close');
 
 // Menu show
 navToggle.addEventListener('click', () =>{
-    navMenu.classList.add('show_menu')
+    navMenu.classList.add('show_menu');
 })
 
 // Menu hidden
 navClose.addEventListener('click', () =>{
-   navMenu.classList.remove('show_menu')
+   navMenu.classList.remove('show_menu');
 })
 
 const shadowHeader = () => {
@@ -52,6 +77,46 @@ const shadowHeader = () => {
     this.scrollY >= 50 ? header.classList.add('shadow_header') : header.classList.remove('shadow_header');
 }
 window.addEventListener('scroll', shadowHeader);
+
+// Light and dark theme
+const themeBtn = document.getElementById('theme_btn'),
+      darkTheme = 'dark_theme',
+      iconSun = 'fa-sun',
+      iconMoon = 'fa-moon';
+
+// Previously selected theme (if user selected)
+const selectedTheme = localStorage.getItem('selected_theme'),
+      selectedIcon = localStorage.getItem('selected_icon');
+
+// Obtain the current theme and icon based on the class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light',
+      getCurrentIcon = () => themeBtn.classList.contains(iconMoon) ? 'fa-moon' : 'fa-sun';
+
+// If a theme was previously selected
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeBtn.classList[selectedIcon === 'fa-moon' ? 'add' : 'remove'](iconMoon);
+    themeBtn.classList[selectedIcon === 'fa-sun' ? 'add' : 'remove'](iconSun);
+}
+
+// Toggle theme and icon manually
+themeBtn.addEventListener('click', () => {
+    // Toggle the theme
+    document.body.classList.toggle(darkTheme);
+    
+    // Toggle the icon classes
+    if (themeBtn.classList.contains(iconMoon)) {
+        themeBtn.classList.remove(iconMoon);
+        themeBtn.classList.add(iconSun);
+    } else {
+        themeBtn.classList.remove(iconSun);
+        themeBtn.classList.add(iconMoon);
+    }
+
+    // Save the current theme and icon in localStorage
+    localStorage.setItem('selected_theme', getCurrentTheme());
+    localStorage.setItem('selected_icon', getCurrentIcon());
+});
 
 /* == == == Main == == == */
 // Hero
